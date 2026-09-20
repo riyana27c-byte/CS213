c8c4aaff25fdda4ebbd9b433ee89590066ee874c
Riyana Chaudhary <riyana27.c@gmail.com>
2026-09-19 23:18:35 -0400

    my classes made

---
+++ src/History.java
@@ -0,0 +1,4 @@
+public class History {
+    private Parking parking; //the parking activity
+    private History next; //reference to the next node in list
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,5 @@
+public class Vehicle {
+    private String plate; //exactly 7 characters
+    private History history; //a singly linked list
+    public void printHistory() //print parking history
+}
