ddd00c273e11abd91a4f91ab1b0cf900e40b4eee
Riyana Chaudhary <riyana27.c@gmail.com>
2026-09-21 16:38:19 -0400

    NEED TO FINISH main and test cases

--- src/Timestamp.java
+++ src/Timestamp.java
@@ -1,2 +1,45 @@
-public class Timestamp {
+public class Timestamp implements Comparable <Timestamp> {
+    private Date date;
+    private byte hour;
+    private byte minute;
+
+    public Timestamp(Date date, byte hour, byte minute){
+        this.date = date;
+        this.hour = hour;
+        this.minute = minute;
+    }
+    @Override
+    public int compareTo(Timestamp o) {
+        if (date.compareTo(o.date) < 0){
+            return -1;
+        }
+        if(date.compareTo(o.date) > 0){
+            return 1;
+        }
+        if(hour > o.hour){
+            return 1;
+        }
+        if(hour < o.hour){
+            return -1;
+        }
+        if(minute > o.minute){
+            return 1;
+        }
+        if(minute < o.minute){
+            return -1;
+        }
+        return 0;
+    }
+
+
+    @Override
+    public String toString() {
+        result = "";
+        result += date.toString();
+        result += " " + hour + ":" + minute;
+        return result;
+    }
 }
+    void main() {
+    }
+
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,5 +1,16 @@
 public class Vehicle {
     private String plate; //exactly 7 characters
     private History history; //a singly linked list
-    public void printHistory() //print parking history
+    public void printHistory(){
+
+    } //print parking history
+    @Override
+    public boolean equals(Object obj){
+
+    }
+    @Override
+    public String toString(){
+
+    }
+
 }
